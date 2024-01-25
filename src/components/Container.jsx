041a72ab76c172367f5a7bdf90de7da3da48d932
ritu raj <alchemyrecloak@gmail.com>
2024-01-25 20:13:42 +0530

    completed the task

---
+++ src/components/Container.jsx
@@ -0,0 +1,15 @@
+
+
+import { FaArrowRight } from "react-icons/fa6";
+import './container.css'
+
+function Container() {
+    return ( <div className="container">
+        <div className="arrowCont">
+            <FaArrowRight/>
+        </div>
+        <div className="text_cont">ASK US HOW</div>
+    </div> );
+}
+
+export default Container;
